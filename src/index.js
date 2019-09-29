783a1e17810f52f625f65d0bf3525ef2d1194f1f
Merna-Zakaria <merna.zakaria25@gmail.com>
2019-09-29 16:06:50 +0200

    test

--- src/index.js
+++ src/index.js
@@ -30,3 +30,9 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
+
+
+
+    // "test": "react-scripts test",
---
+++ src/testing/App.spec.js
@@ -0,0 +1,12 @@
+describe('My Test Suite', () => {
+    it('My Test Case', () => {
+      expect(true).toEqual(true);
+    });
+  });
+
+
+  describe('equality', () =>{
+      it('must equal', () =>{
+          expect()
+      })
+  })
